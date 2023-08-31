87bc21efcc8119cdcf02ecd69dbb4ff085580d82
Jose Monge <services@josemonge.dev>
2023-08-31 12:47:52 -0600

    Fixed incorrect port configuration on server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
-const { ApolloServer } = require("@apollo/server");
-const { startStandaloneServer } = require("@apollo/server/standalone");
-const typeDefs = require("./schema");
-const resolvers = require("./resolvers");
-const TrackAPI = require("./datasources/track-api");
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const TrackAPI = require('./datasources/track-api');
 
 async function startApolloServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers,
   });
-
-  await server.listen({ port: process.env.PORT || 4000 });
 
   const { url } = await startStandaloneServer(server, {
     context: async () => {
@@ -19,6 +17,9 @@ async function startApolloServer() {
           trackAPI: new TrackAPI(),
         },
       };
+    },
+    listen: {
+      port: process.env.PORT || 4000,
     },
   });
 
